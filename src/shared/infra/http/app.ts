c85ac05ffa6e3e72497786805377dd87667678ba
jhongomes <jhonatangomes859@gmail.com>
2022-01-08 23:26:04 -0400

    adicionando swagger

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,15 +2,19 @@ import "reflect-metadata";
 import "dotenv/config"
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+
 import "../database"
 import "../../container"
 import upload from "../../../config/upload"
 import { AppError } from "../../errors/AppError";
+import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/images", express.static(`${upload.tmpFolder}/imagesUrl`));
 
 app.use(router);
